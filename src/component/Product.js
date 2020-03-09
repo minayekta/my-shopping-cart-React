f843a11d215b396447f849d03ed85792f711505d
Mina Qolizadeh <mina.qolizadeh@hotmail.com>
2020-03-09 15:11:53 +0330

    add reactstrap in to the project

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,12 +1,27 @@
 import React from  'react';
-
+import {
+Card,
+CardImg,
+CardBody,
+CardTitle,
+CardSubtitle,
+Button
+       
+} from 'reactstrap';
 
 
 class Product extends React.Component{
     render(){
       const productItems = this.props.products.map(product =>
             <div className="col-md-4 text-center product" key={product.id}>
-            
+            {/* <Card> 
+              <CardImg  top width="100%" src={`/products/0${product.id}.jpg`} alt={product.id} />
+              <CardBody>
+              <CardTitle> {product.title} </CardTitle>
+              <CardSubtitle> T  {product.price} </CardSubtitle>
+              <Button color="primary">افزودن به سبد خرید</Button>
+            </CardBody>
+          </Card> */}
               <div>
                 <img src={`/products/0${product.id}.jpg`} alt={product.id} />
               </div>
